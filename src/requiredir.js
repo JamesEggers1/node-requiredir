cc096b9b641608c5b455fd45c60fc20c152ae031
Joshua Poehls <joshua@zduck.com>
2012-07-11 22:06:37 -0500

    Include the absolute path in 'directory path does not exist' and 'path provided is not a directory' exceptions. This will make debugging these errors in an app much easier.

--- src/requiredir.js
+++ src/requiredir.js
@@ -13,14 +13,18 @@ module.exports = (function(){
 			throw new Error("The path must be provided when instantiating the object.");
 		}
 
+		// Resolve the full path so that if an error occurs
+		// we can show the user the absolute path, for clarity.
+		path = _path.resolve(path);
+
 		try {
 			stats = _fs.statSync(path);
 		} catch (e) {
-			throw new Error("The directory path does not exist.");
+			throw new Error("The directory path does not exist. [" + path + "]");
 		}
 
 		if (!stats.isDirectory()){
-			throw new Error("The path provided is not a directory.");
+			throw new Error("The path provided is not a directory. [" + path + "]");
 		}
 	};
 
