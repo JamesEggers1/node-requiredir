8319604e8ff142bdcf40a9e5cbe7f2429cd28a32
Joshua Poehls <joshua@zduck.com>
2012-07-11 21:54:05 -0500

    Throw Error instead of TypeError. TypeError is 'when a value is not of the expected type', according to MDN.

--- src/requiredir.js
+++ src/requiredir.js
@@ -10,18 +10,17 @@ module.exports = (function(){
 
 		if (typeof path === "undefined"
 			|| (typeof path === "string" && path.trim().length === 0)) {
-			throw new TypeError("The path must be provided when instantiating the object.");
+			throw new Error("The path must be provided when instantiating the object.");
 		}
 
 		try {
 			stats = _fs.statSync(path);
-
 		} catch (e) {
-			throw new TypeError("The directory path does not exist.");
+			throw new Error("The directory path does not exist.");
 		}
 
 		if (!stats.isDirectory()){
-			throw new TypeError("The path provided is not a directory.");
+			throw new Error("The path provided is not a directory.");
 		}
 	};
 
